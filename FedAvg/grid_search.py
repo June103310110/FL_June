115790780d1cecef22a654b8e7b7da58ed9db4a6
june103310110 <june103310110@gmail.com>
2021-09-01 11:42:10 +0800

    update experiment record

--- FedAvg/grid_search.py
+++ FedAvg/grid_search.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-# grid search
-
+# # grid search 
+# https://blog.gtwang.org/programming/tensorflow-object-detection-api-multiple-gpu-parallel-training/
 
 # In[1]:
 
 
-rounds = 10
+rounds = 100
 C = [1, 0.5, 0.1]
 K = [12, 50, 100, 1000]
 E = [1,3,7]
 mu = [1.0,0.0]
 
 
-# In[2]:
+# In[ ]:
 
 
 import pandas as pd
@@ -31,13 +28,13 @@ lis = []
 for c_ in C:
     for k_ in K:
         for e_ in E:
-            
-            cmd = 'python E_FedProx.py --rounds '+str(rounds)+' --k '+str(k_)+' --c '+str(c_)+' --e '+str(e_)
-    #         cmd = 'python E_FedProx.py -h'
-            os.system(cmd)
-#                 print(cmd)
-            df_loc = max([os.path.join(directory,d) for d in os.listdir(directory)], key=os.path.getmtime)
-            lis.append([c_, k_, rounds, e_, df_loc])
+            for mu_ in mu:
+                cmd = 'python E_FedProx.py --rounds '+str(rounds)+' --k '+str(k_)+' --c '+str(c_)+' --e '+str(e_)+' --mu '+str(mu_)
+        #         cmd = 'python E_FedProx.py -h'
+                os.system(cmd)
+    #                 print(cmd)
+                df_loc = max([os.path.join(directory,d) for d in os.listdir(directory)], key=os.path.getmtime)
+                lis.append([c_, k_, rounds, e_, mu,  df_loc])
 
 
 # In[4]:
@@ -56,7 +53,8 @@ df['C'] = to_list(a, 0)
 df['K'] = to_list(a, 1)
 df['rounds'] = to_list(a, 2)
 df['epochs'] = to_list(a, 3)
-df['df_loc'] = to_list(a, 4)
+df['mu'] = to_list(a,4)
+df['df_loc'] = to_list(a, 5)
 
 df.to_csv('grid_search/grid_search_'+timestr+'.csv', index=False)
 
