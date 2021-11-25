60f3bf53af520b66260578698f2cef68ab5ed495
june103310110 <june103310110@gmail.com>
2021-11-25 17:31:44 +0800

    new

---
+++ client/setup.py
@@ -0,0 +1,17 @@
+import os
+
+run_cmd = lambda cmd_lis:[os.popen(i).read() for i in cmd_lis.split('\n')]
+
+if 'federated_aia_test' in os.listdir():
+    cmd_lis = '''cd federated_aia_test
+    git pull
+    '''
+else: 
+    print('update federated_aia_test')
+    cmd_lis = '''git clone https://at102091:12345678@gitlab.aiacademy.tw/junew/federated_aia_test.git
+    cd federated_aia_test
+    git pull
+    '''
+
+run_cmd(cmd_lis)
+
